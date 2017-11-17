b71ed81b34d66071a9045d3be9408ae2f9618a99
Bai <349644478@qq.com>
2017-11-17 10:30:48 +0800

    叮当猫部分代码

--- 绘画/绘画/绘画.cpp
+++ 绘画/绘画/绘画.cpp
@@ -5,13 +5,40 @@
 #include <graphics.h>      // 引用图形库头文件
 #include <conio.h>
 
-
+#define long 640
+#define width 480
+#define PI 3.14159265
 int main()
 {
+
 	initgraph(640, 480);
-	setfillcolor(WHITE);
-	setbkcolor(BLACK);
-	circle(320, 240,20);
+	setbkcolor(WHITE); //背景
+	setfillcolor(WHITE);//身体
+	fillcircle(long / 2, width / 2 + 40, 75);
+	setfillcolor(GREEN);//头部
+	fillcircle(long / 2, width / 2 -50 , 100);
+	setfillcolor(WHITE);//脸部
+	fillellipse(long / 2-90, (width / 2-30)-80, long / 2+90, (width / 2-30)+80);
+	setfillcolor(RED);//鼻子
+	fillcircle(long / 2, width / 2 - 70, 10);
+	setcolor(BLACK);//眼睛
+	roundrect(long / 2 - 40, width / 2 - 120, long / 2 - 5, width / 2 - 65, 45, 45);
+	setcolor(BLACK);//眼睛
+	roundrect(long / 2 + 40, width / 2 - 120, long / 2 + 5, width / 2 - 65, 45, 45);
+	setfillcolor(BLACK);//眼珠子
+	fillcircle(long / 2-10, width / 2 - 85, 4);
+	setfillcolor(BLACK);
+	fillcircle(long / 2 + 10, width / 2 - 85, 4);
+	setfillcolor(BLACK);//胡子
+	line(long / 2-30, width / 2 - 50,long/2-65, width / 2 - 60);
+	line(long / 2 - 30, width / 2 - 45, long / 2 - 65, width / 2 - 45);
+	line(long / 2 - 30, width / 2 - 40, long / 2 - 65, width / 2 - 30);
+	line(long / 2 + 30, width / 2 - 50, long / 2 + 65, width / 2 - 60);
+	line(long / 2 + 30, width / 2 - 45, long / 2 + 65, width / 2 - 45);
+	line(long / 2 + 30, width / 2 - 40, long / 2 + 65, width / 2 - 30);
+	setfillcolor(BLACK);//嘴巴
+	arc(long / 2 -100, width / 2 - 80, long / 2 + 100, width / 2 -80, PI * 5 / 4, PI * 7 / 4);
+	
 	getchar();
     return 0;
 }
